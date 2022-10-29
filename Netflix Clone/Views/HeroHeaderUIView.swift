cfd651e4063a44084a6bc99dddf0bf53cd3da341
tunaytoksoz <tunaytoksoz@hotmail.com>
2022-10-29 15:11:59 +0300

    .

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,33 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Tunay Toksöz on 29.10.2022.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
