ffb93dc18bf097febdf4baee32f6ffac21fb0154
tunaytoksoz <tunaytoksoz@hotmail.com>
2022-10-29 14:18:43 +0300

    MVVM yapısı oluşturuldu. Tabbar tamamlandı.

---
+++ Netflix Clone/Controllers/DownloadaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadaViewController.swift
+//  Netflix Clone
+//
+//  Created by Tunay Toksöz on 29.10.2022.
+//
+
+import UIKit
+
+class DownloadaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+       
+    }
+}
---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Tunay Toksöz on 29.10.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
